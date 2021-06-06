781754f743830f9ee7518775e3991597766881f2
gafarasem <gafarasem255@hotmail.com>
2021-06-06 03:12:28 -0800

    python

--- Send_Email.py
+++ Send_Email.py
@@ -1,12 +1,12 @@
+import smtplib
 from mailer import Mailer
+from mailer import Message
 
+gmail_user = 'gafarasem255@gmail.com'
+gmail_password = '@zz00z255'
 
-#creat clieant api
-mail = Mailer(email='gafarasem255@gmail.com',password='gafarasem622')
-
-
-#send email
-mail.send(receiver='gafarasem255@gmail.com',  # Email From Any service Provider
-          subject='mp3',
-          message='This is th file ',
-          file='')
+server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+server.ehlo()
+server.login(gmail_user, gmail_password)
+server.sendmail('gafarasem255@gmail.com', 'susn622@gmail.com', "ssssss")
+server.close()
--- downloader_playList.py
+++ downloader_playList.py
@@ -1,28 +1,24 @@
-import requests
-import urllib.request
 import time
-from bs4 import BeautifulSoup
-import json
+from selenium import webdriver
 
-def getElementText(name, dict, soup):
-        elements = soup.find(name, dict)
-        print(elements.text)
-        return elements.text
+playlist = input('url of playlist : ')
+web = webdriver.Chrome("chromedriver.exe")
+web.get(playlist)
+numberOFvideos = web.find_element_by_xpath('//*[@id="stats"]/yt-formatted-string[1]/span[1]').text
+print(numberOFvideos)
 
-def getElement(name, dict, soup):
-        elements = soup.find(name, dict)
-        return elements
+one = web.find_elements_by_xpath('//*[@id="video-title"]')
 
-def getElementContent(name, dict, key, soup):
-        elements = soup.find(name, dict)
-        print(elements[key])
-        return elements[key]
+web2 = webdriver.Chrome("chromedriver.exe")
 
-
-playlist_url=requests.get('https://grey.egybest.network/explore/?q=happy')
-soup = BeautifulSoup(playlist_url.content, "html.parser")
-
-dateMovie = getElement('div', {'class': 'movie_title tam'}, soup)
-dateMovie = getElementText('a', {},dateMovie)
-
-print(dateMovie)
+for i in range(len(one)):
+    try:
+        web2.get('https://ar.savefrom.net/' + str(one[i].get_attribute('href')).split('&list')[0])
+        time.sleep(2)
+        web2.find_element_by_xpath('//*[@id="sf_submit"]').click()
+        time.sleep(7)
+        web2.find_element_by_xpath('//*[@id="sf_result"]/div/div/div[2]/div[2]/div[1]').click()
+        time.sleep(1)
+        print(i)
+    except:
+        print(str(one[i].get_attribute('href')).split('&list')[0])
